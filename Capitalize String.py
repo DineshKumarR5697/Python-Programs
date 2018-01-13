9371b1525c67faf40549685b6708dbca117b5452
DineshKumarR5697 <rdinesh5697@gmail.com>
2018-01-13 18:00:56 +0530

    Add files via upload

---
+++ Capitalize String.py
@@ -0,0 +1,5 @@
+name=input('Enter String:')
+caps_name=""
+for ltr in name:
+    caps_name += ltr.upper()
+print(caps_name)
---
+++ String Reverse in five lines.py
@@ -0,0 +1,5 @@
+name=input('Enter String:')
+rev_name=""
+for ltr in name[::-1]:
+    rev_name += ltr
+print(rev_name)
